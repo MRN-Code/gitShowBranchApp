b219e37a21ca906c1771818be00a5bf34085b733
Dylan Wood <dwood@lintcoin.mind.unm.edu>
2014-07-11 00:56:44 -0600

    working :-)

--- public/javascripts/utils.js
+++ public/javascripts/utils.js
@@ -7,32 +7,72 @@
         }
     }
 
-    function getBranchCommits () {
-        $.getJSON('showbranch?callback=?', function(data) {
-            data.forEach(function(repo){
+    function startLoading () {
+        $('.loading-animation').addClass('la-animation').fadeIn(100);
+        $('.loading-message').fadeIn(100);
+    }
+    function stopLoading () {
+        $('.loading-animation').fadeOut(100, function(){$('.loading-animation').removeClass('la-animation');});
+        $('.loading-message').fadeOut(100);
+    }
+
+    function getReleases() {
+        $.getJSON('showbranch')
+            .done(function (data) {
                 var container = $('<div>');
-                container.append('<h3>' + repo.repo + '</h3>');
+                container.append('<h3>Select a release</h3>');
                 container.append('<table cellpadding="0" cellspacing="0" border="0" class="cell-border hover"></table>');
-                $('body').append(container);
+                $('#content').html('').append(container);
                 var dataTableConfig = {
-                    columns: [{title: "Commit", width:"40%"}]
-                        .concat(
-                            repo.diff.branches.map(
-                                function extractLabel (branch) {
-                                    return {title: branch.label};
-                                }
-                            )
-                        ),
-                    data: repo.diff.commits.map(function buildRows(commit) {
-                        return ['<a href="https://github.com/MRN-Code/' + repo.repo + '/commit/' + commit.label + '" target="_blank">[' + commit.label + ']</a> ' + commit.commitMessage].concat(commit.branches.map(convertToImage));
+                    columns: [{title: "Releases"}],
+                    data: data.map(function buildLinks(release) {
+                        return ['<a href="#release/' + release + '">' + release + '</a>'];
                     })
                 };
                 container.children('table').dataTable(dataTableConfig);
+            }).fail(function (err) {
+                    $('#content').html('<h3 class="error">Error fetching releases</h3>');
+                    console.assert(false, err);
             });
-            $('.loading-animation').fadeOut(function(){$('.loading-animation').removeClass('la-animation');});
-            $('.loading-message').hide();
-        });
+    }
+    function getBranchCommits (release) {
+        if(location.hash.match(/release\/.*/)) {
+            var release = location.hash.match(/release\/(.*)$/)[1];
+            $.getJSON('showbranch/release/' + release)
+                .done(function(data) {
+                    $('#content').html('');
+                    data.forEach(function(repo){
+                        var container = $('<div>');
+                        container.append('<h3>' + repo.repo + '</h3>');
+                        container.append('<table cellpadding="0" cellspacing="0" border="0" class="cell-border hover"></table>');
+                        $('#content').append(container);
+                        var dataTableConfig = {
+                            columns: [{title: "Commit", width:"40%"}]
+                                .concat(
+                                    repo.diff.branches.map(
+                                        function extractLabel (branch) {
+                                            return {title: branch.label};
+                                        }
+                                    )
+                                ),
+                            data: repo.diff.commits.map(function buildRows(commit) {
+                                return ['<a href="https://github.com/MRN-Code/' + repo.repo + '/commit/' + commit.label + '" target="_blank">[' + commit.label + ']</a> ' + commit.commitMessage].concat(commit.branches.map(convertToImage));
+                            })
+                        };
+                        container.children('table').dataTable(dataTableConfig);
+                    });
+                }).fail(function (err) {
+                    $('#content').html('<h3 class="error">Error fetching release logs</h3>');
+                    console.assert(false, err);
+                });
+         } else {
+            getReleases();
+         }
      };
+
+     $(document).ajaxStart(startLoading);
+     $(document).ajaxStop(stopLoading);
+     window.addEventListener('hashchange', getBranchCommits);
 
      $(document).ready(getBranchCommits);
 })();
--- routes/showbranch.js
+++ routes/showbranch.js
@@ -1,20 +1,41 @@
+// requirements
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+var logDir = '../coins_builder/log/show_branch';
+var RSVP = require ('rsvp');
+var url = require('url');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-    var url = require('url');
+    fs.readdir(logDir, function(err, logDirs) {
+        res.end(JSON.stringify(logDirs));
+    });
+}); 
+
+router.param('release', function (req, res, next, releaseDir) {
+    fs.readdir(logDir, function(err, logDirs) {
+        if (err) {
+            next(err);
+        } else if (logDirs.indexOf(releaseDir) !== -1) {
+            req.releaseDir = releaseDir;
+            next();
+        } else {
+            next(new Error('unknown release specified: ' + releaseDir));
+        }
+    });
+});
+
+router.get('/release/:release', function(req, res) {
     var parser = require('git-show-branch-parser');
-    var fs = require('fs');
-    var RSVP = require ('rsvp');
-    var logDir = '../coins_builder/log/show_branch/current';
+    var releaseDir = logDir +  '/' + req.releaseDir;
 
     RSVP.on('error', function(reason) {
         console.assert(false, reason);
     });
 
-    fs.readdir(logDir, function(err, logs) {
-        var parsePromises = logs.map(function(log){return parser(logDir + '/' + log)});
+    fs.readdir(releaseDir, function(err, logs) {
+        var parsePromises = logs.map(function(log){return parser(releaseDir + '/' + log)});
         RSVP.Promise.all(parsePromises)
             .then(function(allParsedObjects) {
                 var logData = logs.map(function(logName, index){
@@ -26,7 +47,7 @@ router.get('/', function(req, res) {
                 var url_parts = url.parse(req.url, true);
                 var query = url_parts.query;
                 var cb = query.callback;
-                res.end(cb + '(' + JSON.stringify(logData) + ')');
+                res.end(JSON.stringify(logData));
             });
     });
 });
